e4007ec4ec1e15082f7ae3446de9171c8750a3a6
Rayanne B. Lima <rayanne_lima2010@hotmail.com>
2022-08-15 11:35:39 -0300

    att: about me text

--- src/components/sections/AboutMe/index.tsx
+++ src/components/sections/AboutMe/index.tsx
@@ -14,16 +14,16 @@ const AboutMe: React.FC = () => {
           <Illustration />
           <AboutMeText>
           <p>
-            Aos 24 anos, sou desenvolvedora desde 2018 e
+            Aos 25 anos, sou desenvolvedora desde 2018 e
             formanda em Ciência da
             Computação no Instituto Federal do Sudeste de
             Minas Gerais com mais de 2 anos de experiência no Programa de Educação Tutorial (PET) de Ciência da Computação.
           </p>
           <p>
-            Concludente como bolsista no curso de Desenvolvimento FullStack Java pelo Santander Coders na Digital House Brasil.
+            Concludente como bolsista no curso de Desenvolvimento FullStack Java pelo Santander Coders na Digital House Brasil e pós graduanda em Análise de Desenvolvimento de Sistemas Computacionais.
           </p>
           <p>
-            Atualmente sou desenvolvedora Web FullStack na Tek-System Informática, membro da EmComp do IF Sudeste e freelancer em projetos Web.
+            Atualmente sou desenvolvedora Front-End Sênior na Tray, desenvolvedora Front-End voluntária na Lacrei, freelancer construindo a plataforma Web da Health Easy e desenvolvedora Front-End para um projeto internacional da Engenharia Digital.
           </p>
           <div>
             <Button linkTo="https://drive.google.com/drive/folders/1r1uh8oxsDG4BGBVtT7tnUlgg17IHRibb?usp=sharing" isOutlined isExternalLink>Certificados</Button>
